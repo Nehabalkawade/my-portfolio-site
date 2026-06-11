53eb56cbdca5a222ed1b01335955e070fe1cae5e
Nehabalkawade <balkawadeneha261@gmail.com>
2026-06-11 11:01:13 +0530

    Update app.py

--- .github/workflows/multi.py
+++ .github/workflows/multi.py
@@ -1,5 +1,21 @@
-numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+numbers = [12, 45, 7, 89, 23, 56,5,7,4,2,8,2,8,32,42,3,7,4,1,14]
 
-print("Lowest number:", min(numbers))
-print("Highest number:", max(numbers))
-print("Average:", sum(numbers) / len(numbers))
+lowest = numbers[0]
+highest = numbers[0]
+total = 0
+
+for num in numbers:
+    if num < lowest:
+        lowest = num
+
+    if num > highest:
+        highest = num
+
+    total = total + num
+
+average = total / len(numbers)
+
+print("Numbers:", numbers)
+print("Lowest number:", lowest)
+print("Highest number:", highest)
+print("Average:", average)
