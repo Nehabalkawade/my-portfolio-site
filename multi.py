250bf910bcf14698165708b0bc8243453eb48f18
Nehabalkawade <balkawadeneha261@gmail.com>
2026-06-11 11:37:58 +0530

    Create multi.py

---
+++ multi.py
@@ -0,0 +1,10 @@
+numbers = [10, 25, 5, 40, 15,34,2,34,5,8,11,34,8,6,35,9,44,22,6,53]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Highest number:", highest)
+print("Lowest number:", lowest)
+print("Average:", average)
